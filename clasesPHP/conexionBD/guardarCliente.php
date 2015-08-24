3e1318425adc3fbbe7b82359dd66f092ee97db14
themarcoszone <themarcoszone@gmail.com>
2015-08-24 00:22:24 -0300

    Update guardarCliente.php

--- clasesPHP/conexionBD/guardarCliente.php
+++ clasesPHP/conexionBD/guardarCliente.php
@@ -9,7 +9,9 @@ $query = "INSERT INTO cliente (nombre,telefono,mail) VALUES('" . $nombre ."','" 
 
 if (mysql_query($query)) {
     echo "El cliente fue agregado exitosamente";
+    header("Refresh: 3; url=cargaCliente.html");
 } else {
     echo "Error con la query: " . $query . "<br>" . mysql_error();
+    header("Location: /sisFacturacion/error.html");
 }
 ?>
