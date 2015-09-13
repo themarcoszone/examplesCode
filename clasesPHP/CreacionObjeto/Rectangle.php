359d2b32d254aebf3ece6de2962e435f033a36db
themarcoszone <themarcoszone@gmail.com>
2015-09-13 16:03:31 -0300

    Create Rectangle.php

---
+++ clasesPHP/CreacionObjeto/Rectangle.php
@@ -0,0 +1,29 @@
+<?php
+class Rectangle{
+	private $x;
+	private $y;
+	private $height;
+	private $width;
+
+	public function __construct($width=10, $height=10, $x=0, $y=0){
+		$this->width = $width;
+		$this->height = $height;
+		$this->x = $x;
+		$this->y = $y;
+	}
+
+	public function getArea(){
+		return $this->height*$this->width;
+	}
+
+	public function printInfo(){
+		printf("Area ".($this->height * $this->width)." xPos = ".$this->x." yPos = ".$this->y."<br/>");
+	}
+
+	public function prueba($x){
+		echo "valor de height ".$x->height;
+	}
+	
+}
+
+?>
